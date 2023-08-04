caf54ca7b12d4ebb6f5e6d36e555213f8b6a8dff
behlock <behlocks@gmail.com>
2023-08-04 15:56:09 +0200

    add awscli2

--- packages.nix
+++ packages.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   home.packages = [
     pkgs.act # run github actions locally
+    pkgs.awscli2 # aws cli
     pkgs.bat # cat clone with syntax highlighting and Git integration
     pkgs.bore-cli # TCP tunnels
     pkgs.comma # run programs without installing them
