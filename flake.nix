bed6782b51a4f8f0ca280482b079090b8c0a9832
behlock <behlocks@gmail.com>
2023-08-14 11:52:24 +0100

    add work macbook profile

--- flake.nix
+++ flake.nix
@@ -22,18 +22,28 @@
       armv7l-linux = home-manager.defaultPackage.armv7l-linux;
     };
 
+    #  Personal Macbook
     homeConfigurations."wbehlock" =
       home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-darwin;
         modules = [ ./home-wbehlock.nix ];
       };
 
+    # Mac Mini
     homeConfigurations."behlock" =
       home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.aarch64-darwin;
         modules = [ ./home-behlock.nix ];
       };
 
+    # Work Macbook
+    homeConfigurations."wbehlock" =
+      home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+        modules = [ ./home-behlock.nix ];
+      };
+
+    # Raspberry Pi
     homeConfigurations."pi" =
       home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.armv7l-linux;
