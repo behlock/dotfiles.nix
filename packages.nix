126badaf13a811d8e0b32da48893c25034c41303
behlock <behlocks@gmail.com>
2023-10-12 16:04:59 +0100

    Add pip

--- packages.nix
+++ packages.nix
@@ -32,6 +32,7 @@
     pkgs.postgresql_jit # postgres
     pkgs.pre-commit # framework for managing and maintaining multi-language pre-commit hooks
     pkgs.python311 # Python 3.11
+    pkgs.python311Packages.pip # python package manager
     pkgs.qpdf # PDF transformation
     pkgs.redis # key-value store
     pkgs.ripgrep # grep alternative
