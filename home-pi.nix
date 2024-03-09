492a6edd2ad4b234b93b2bbfa84b8b6a68bf5986
behlock <behlocks@gmail.com>
2024-03-09 17:07:17 +0000

    Upgrade home manager to 23.11

--- home-pi.nix
+++ home-pi.nix
@@ -2,7 +2,7 @@
   home.username = "behlock";
   home.homeDirectory =
     "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/pi";
-  home.stateVersion = "22.11";
+  home.stateVersion = "23.11";
   programs.home-manager.enable = true;
 
   imports = [ ./packages.nix ./vim.nix ./git.nix ./rest.nix ];
--- home-wbehlock.nix
+++ home-wbehlock.nix
@@ -2,7 +2,7 @@
   home.username = "wbehlock";
   home.homeDirectory =
     "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/wbehlock";
-  home.stateVersion = "22.11";
+  home.stateVersion = "23.11";
   programs.home-manager.enable = true;
 
   imports = [ ./packages.nix ./vim.nix ./git.nix ./rest.nix ];
