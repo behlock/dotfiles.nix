f887c5c9d2d69d4b01dc76abed40e59517f1844b
behlock <behlocks@gmail.com>
2024-12-05 09:34:57 +0000

    allow unfree packages

--- home-wbehlock.nix
+++ home-wbehlock.nix
@@ -4,6 +4,7 @@
     "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/wbehlock";
   home.stateVersion = "24.05";
   programs.home-manager.enable = true;
+  nixpkgs.config.allowUnfree = true;
 
   imports = [ ./packages.nix ./vim.nix ./git.nix ./rest.nix ];
 }
