88d94b93a1d2f1960f1f03633074bb3605dc7008
behlock <behlocks@gmail.com>
2026-01-12 17:25:07 +0000

    lazygit theme handling

--- git.nix
+++ git.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   programs.git = {
     enable = true;
     lfs.enable = true;
@@ -17,22 +17,45 @@
     enable = true;
     enableGitIntegration = true;
     options = {
-      "syntax-theme" = "base16";
-      "minus-style" = "syntax #2a2a2a";
-      "minus-emph-style" = "syntax #3a3a3a";
-      "plus-style" = "syntax #2a2a2a";
-      "plus-emph-style" = "syntax #3a3a3a";
       "line-numbers" = true;
-      "line-numbers-minus-style" = "#666666";
-      "line-numbers-plus-style" = "#888888";
-      "line-numbers-zero-style" = "#444444";
       "line-numbers-left-format" = "{nm:>4} ";
       "line-numbers-right-format" = "{np:>4} ";
       "hunk-header-style" = "omit";
-      "file-style" = "#ffffff bold";
       "file-decoration-style" = "none";
+      # Dark mode settings (default)
+      "dark" = true;
+      "syntax-theme" = "base16";
+      "minus-style" = "syntax #3a2a2a";
+      "minus-emph-style" = "syntax #4a3030";
+      "plus-style" = "syntax #2a3a2a";
+      "plus-emph-style" = "syntax #304a30";
+      "line-numbers-minus-style" = "#666666";
+      "line-numbers-plus-style" = "#888888";
+      "line-numbers-zero-style" = "#444444";
+      "file-style" = "#ffffff bold";
     };
   };
+
+  # Delta wrapper script that auto-detects light/dark mode
+  home.packages = [
+    (pkgs.writeShellScriptBin "delta-auto" ''
+      if [[ "$(defaults read -g AppleInterfaceStyle 2>/dev/null)" == "Dark" ]]; then
+        exec delta --dark --syntax-theme=base16 \
+          --minus-style='syntax #3a2a2a' --plus-style='syntax #2a3a2a' \
+          --minus-emph-style='syntax #4a3030' --plus-emph-style='syntax #304a30' \
+          --line-numbers-minus-style='#666666' --line-numbers-plus-style='#888888' \
+          --line-numbers-zero-style='#444444' --file-style='#ffffff bold' \
+          "$@"
+      else
+        exec delta --light --syntax-theme=GitHub \
+          --minus-style='syntax #ffebe9' --plus-style='syntax #e6ffec' \
+          --minus-emph-style='syntax #ffc0c0' --plus-emph-style='syntax #a0f0a0' \
+          --line-numbers-minus-style='#cf222e' --line-numbers-plus-style='#1a7f37' \
+          --line-numbers-zero-style='#666666' --file-style='#000000 bold' \
+          "$@"
+      fi
+    '')
+  ];
   programs.lazygit = {
     enable = true;
     settings = {
@@ -40,7 +63,7 @@
         pagers = [
           {
             colorArg = "always";
-            pager = "delta --dark --paging=never --syntax-theme=base16 --minus-style='syntax #2a2a2a' --plus-style='syntax #2a2a2a' --minus-emph-style='syntax #3a3a3a' --plus-emph-style='syntax #3a3a3a' --line-numbers-minus-style='#666666' --line-numbers-plus-style='#888888'";
+            pager = "delta-auto --paging=never";
           }
         ];
       };
